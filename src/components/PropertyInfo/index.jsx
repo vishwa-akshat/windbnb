afcbe715d128eb79413dcee2e16682a58bf651ae
vishwa-akshat <akvs0707@gmail.com>
2023-04-23 13:49:11 +0530

    feat: property card added

--- src/components/PropertyInfo/index.jsx
+++ src/components/PropertyInfo/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import PropertyType from "src/components/PropertyType";
 import PropertyRating from "src/components/PropertyRating";
+import SuperHostTag from "src/components/SuperHostTag";
 
 import "./style.scss";
 
@@ -10,6 +11,7 @@ export default function PropertyInfo({ propertyData }) {
     return (
         <div className="property-info-wrapper">
             <div className="property-info">
+                {propertyData.superHost ? <SuperHostTag /> : null}
                 <PropertyType
                     type={propertyData.type}
                     beds={propertyData.beds}
---
+++ src/components/SuperHostTag/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./style.scss";
+
+export default function SuperHostTag() {
+    return <div className="super-host-tag">Super Host</div>;
+}
